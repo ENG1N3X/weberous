f1d1369a8d7e62bfc4e12f29db7c01f6c480d2ca
eng1n3x <eng1n3x.developer@gmail.com>
2020-09-04 16:12:01 +0300

    +build

--- build/assets/theme.js
+++ build/assets/theme.js
@@ -1,10 +1,5 @@
 "use strict";
 
 $(document).ready(function () {
-  console.log("test1");
-});
-"use strict";
-
-$(document).ready(function () {
-  console.log("test2");
+  $(document).foundation();
 });
